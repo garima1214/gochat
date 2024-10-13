b11a1745d69bc748c70f20c6d712f850903cdbc7
Garima <garimachopra1214@gmail.com>
2024-10-13 22:10:59 +0530

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import app from "./app.js";
-
+import logger from "../configs/logger.config.js";
 //dot env config
 
 //env variables
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`server is listening ${PORT}`);
+  logger.info(`server is listening ${PORT}`);
 });
